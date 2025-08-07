896feaca573882e90e13949ff6208b4e11b05fdf
herseyc <Hersey Cartwright>
2025-08-07 09:30:54 -0400

    Config Backups

---
+++ uploadsuffolksky.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Uploads latest.jpg from All Sky Camera to SuffolkSky.com
+#
+# This is run via cron every 5 minutes
+# */5 * * * * /home/pi/uploadsuffolksky.sh
+
+# use sftp to copy the latest.jpg to suffolksky.com
+
+sftp -i PATHTOAWS.pem USER@www.suffolksky.com:/home/bitnami/stack/wordpress/ <<< $'put /var/www/html/allsky/images/latest.jpg'
+
